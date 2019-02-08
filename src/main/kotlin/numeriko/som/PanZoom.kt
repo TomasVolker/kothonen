f21f5cf6ebda0b277ddc49730d6aa9c1e16befd7
tomasvolker <tomasvolker@gmail.com>
2019-02-08 14:34:06 +0100

    Fix pan and zoom bug

--- src/main/kotlin/numeriko/som/PanZoom.kt
+++ src/main/kotlin/numeriko/som/PanZoom.kt
@@ -1,25 +1,37 @@
 package numeriko.som
 
+import numeriko.openrndr.toHomogeneous
+import numeriko.openrndr.xy
 import org.openrndr.Extension
 import org.openrndr.MouseEvent
 import org.openrndr.Program
 import org.openrndr.draw.Drawer
 import org.openrndr.math.Matrix44
+import org.openrndr.math.Vector2
+import org.openrndr.math.Vector4
 import org.openrndr.math.transforms.transform
+import kotlin.math.exp
 
 private class Camera2D {
+
+    var scrollSpeed: Double = 0.1
+
     var view = Matrix44.IDENTITY
+
+    fun cameraToWorld(position: Vector2) = (view.inversed * position.vector3().toHomogeneous()).xy
+    fun worldToCamera(position: Vector2) = (view * position.vector3().toHomogeneous()).xy
+
     fun mouseDragged(event: MouseEvent) {
         view *= transform { translate(event.dragDisplacement / view[0].x) }
     }
 
     fun mouseScrolled(event: MouseEvent) {
-        val delta = view.inversed * event.position.vector3(z = 1.0)
+        val worldPosition = cameraToWorld(event.position)
 
         view *= transform {
-            translate(delta)
-            scale(1.0 + event.rotation.y * 0.01)
-            translate(-delta)
+            translate(worldPosition)
+            scale(exp(scrollSpeed * event.rotation.y))
+            translate(-worldPosition)
         }
     }
 }
@@ -29,7 +41,9 @@ class PanZoom : Extension {
     override var enabled: Boolean = true
 
     private val camera = Camera2D()
+
     override fun setup(program: Program) {
+
         program.mouse.dragged.listen {
             if (!it.propagationCancelled) {
                 camera.mouseDragged(it)
@@ -41,6 +55,7 @@ class PanZoom : Extension {
                 camera.mouseScrolled(it)
             }
         }
+
     }
     override fun beforeDraw(drawer: Drawer, program: Program) {
         drawer.view = camera.view
